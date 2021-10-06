ba0dd9edd982ca5e60d9832e6e6ddb1b8c4e6b8d
polinav2020 <polinapiksi2019@mail.ru>
2021-10-06 23:59:07 +0300

    feat: add reducer

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-const Genres = (props) => {
-  return;
-  <div>
-    <h1>There will be users Genres</h1>
-    <button>Добавить в коллекцию</button>
-  </div>;
+const Genres = () => {
+  const dispatch = useDispatch();
+
+  const number = useSelector((state) => {
+    return state.number;
+  });
+
+  const addToCollectionAction = {
+    type: "BUTTON_ADD_TO_COLLECTION_PRESSED"
+  };
+
+  const addToCollection = () => {
+    dispatch(addToCollectionAction);
+  };
+
+  return (
+    <div>
+      <h1>There will be users Genres</h1>
+      <button
+        onClick={() => {
+          addToCollection;
+        }}
+      >
+        Добавить в коллекцию
+      </button>
+    </div>
+  );
 };
 
 export default Genres;
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,6 +1,13 @@
-import React from "react";
+const initialState = {
+  number: 0
+};
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
+  if (action.type === "BUTTON_ADD_TO_COLLECTION_PRESSED") {
+    return {
+      number: state.number + 1
+    };
+  }
   return state;
 };
 
